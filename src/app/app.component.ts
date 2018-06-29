96b2c4d4942e0e561767eab91e4112d6b862faa4
Andrew Smith <smith.andrew.l@gmail.com>
2018-06-29 12:45:40 -0500

    Connected button to api call.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {DataServiceService} from '../app/data-service.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataServiceService]
 })
 export class AppComponent {
+
+  constructor(private dataService: DataServiceService) {
+
+  }
+
   title = 'app';
+
+  onClick() {
+    
+    this.dataService.getByPostalCode('63755').subscribe(response => alert(JSON.stringify(response.json())));
+    
+  }
 }
---
+++ src/app/data-service.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers, Response} from "@angular/http";
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  private baseUrl: string = "http://api.geonames.org/";
+  private apiKey: string = "smithandrewl";
+
+    constructor(public http: Http) {
+    }
+  
+    public getByPostalCode(postalCode:string):  Observable<Response> {
+      
+        return this.http.get(
+          this.baseUrl + "postalCodeSearchJSON", 
+          {
+            params: {
+              postalcode : "9011",
+              maxRows    : "10",
+              username   : this.apiKey
+            }
+          }
+        );
+    }
+}
